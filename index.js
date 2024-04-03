227c9844083aaa195857f75334fbe0a2ff967df2
Ela110 <156794040+Ela110@users.noreply.github.com>
2024-04-03 11:50:57 +0200

    new index file

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const jsonData = require('./pokemon.json')
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/pokemon", (req, res) => {
+    res.json(jsonData);
+//   res.send('This is Pokemon');
+});
+
+app.get("/pokemon/:id", (req, res) => {
+    // die variable id ist der requierte parameter
+    const { id } = req.params;
+    //find map:
+    const poke = jsonData.find((poke) => {
+        //jsonData is where I search for my info
+      return poke.id == id;
+    });
+    if (!poke) return  res.status(404).json({ message: "NOT FOUND"});
+    res.json(poke);
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
